8d2ab29e02e5695272c07a4e19ae304c9f847247
GRLost <wangtianxin2000@msn.com>
2024-06-04 17:56:00 +0800

    update MathJax

--- scripts/extension.js
+++ scripts/extension.js
@@ -17,33 +17,7 @@ const init = () => {
 	document.body.appendChild(upper);
 
 	body.childNodes.forEach(node => {
-		if (!!node.tagName) {
-			if (node.classList.contains('mathjax')) return;
-			let content = node.innerHTML;
-			if (!content) return;
-			let poses = [];
-			content.replace(/\\?\$+/g, (m, pos) => {
-				var hasPre = false, len = m.length;
-				if (m.indexOf('\\') === 0) {
-					len --;
-					hasPre = true;
-				}
-				if (hasPre || len > 1) return;
-				poses.push(pos);
-			});
-			if (poses.length === 0) return;
-			let len = poses.length;
-			if (len >> 1 << 1 !== len) return;
-			for (let i = len - 1; i >= 0; i -= 2) {
-				let bra = poses[i - 1], ket = poses[i] + 1;
-				let ctx = content.substring(bra, ket);
-				let pre = content.substring(0, bra);
-				let post = content.substring(ket);
-				content = pre + '<span class="mathjax inner">' + ctx + '</span>' + post;
-			}
-			node.innerHTML = content;
-		}
-		else {
+		if (!node.tagName) {
 			let content = node.data;
 			content = content.replace(/^\s*|\s*$/g, '');
 			if (!content) return;
